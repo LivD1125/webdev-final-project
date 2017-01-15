a66b046dc2674f02eb7b9a48e367a54f173515ac
Olivia DeFlumeri <livd1125@gmail.com>
2017-01-15 11:45:30 -0500

    some small typos

--- test/app.js
+++ test/app.js
@@ -7,11 +7,11 @@ module.exports = function(app)
     var connectionString = 'mongodb://127.0.0.1:27017/test';
 
     if(process.env.MLAB_USERNAME) {
-      connectionString = process.env.MLAB_USERNAME + ":" +
-      process.env.MLAB_PASSWORD + "@" +
-      process.env.MLAB_HOST + ':' +
-      process.env.MLAB_PORT + '/' +
-      process.env.MLAB_APP_NAME;
+        connectionString = process.env.MLAB_USERNAME + ":" +
+            process.env.MLAB_PASSWORD + "@" +
+            process.env.MLAB_HOST + ':' +
+            process.env.MLAB_PORT + '/' +
+            process.env.MLAB_APP_NAME;
     }
 
     var mongoose = require("mongoose");
@@ -28,7 +28,7 @@ module.exports = function(app)
             .find()
             .then(
                 function(tests) {
-                    res.json(testsssF);
+                    res.json(tests);
                 },
                 function(err) {
                     res.status(400).send(err);
